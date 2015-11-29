85219ced5fb5b451ff9db72cab8756d62944399d
Artem Prigoda <arteamon@gmail.com>
2015-11-29 20:18:23 +0300

    Correctly set routes for the root vertex

--- src/main/java/TheTwoRoutes.java
+++ src/main/java/TheTwoRoutes.java
@@ -19,6 +19,10 @@ public class TheTwoRoutes {
                 if (graph.get(candidate, i) == Integer.MAX_VALUE) {
                     graph.set(candidate, i, graph.get(root, candidate) + 1);
                     graph.set(i, candidate, graph.get(root, candidate) + 1);
+                    if (graph.get(root, i) == 0) {
+                        graph.set(root, i, graph.get(root, candidate) + 1);
+                        graph.set(i, root, graph.get(root, candidate) + 1);
+                    }
                     candidates.add(i);
                 }
             }
@@ -41,18 +45,33 @@ public class TheTwoRoutes {
             return vertexes[i][j];
         }
 
-        public void set(int i, int j, int value) {
-            vertexes[i][j] = value;
+        public void uSet(int i, int j, int value) {
+            set(i, j, value);
+            set(j, i, value);
         }
 
-        public int[] adjacent(int i) {
-            return vertexes[i];
+        public void set(int i, int j, int value) {
+            vertexes[i][j] = value;
         }
 
         public Graph copy() {
             Graph copy = new Graph(length());
             for (int i = 0; i < length(); i++) {
                 System.arraycopy(vertexes[i], 0, copy.vertexes[i], 0, length());
+            }
+            return copy;
+        }
+
+        public Graph reverse() {
+            Graph copy = copy();
+            for (int i = 0; i < length(); i++) {
+                for (int j = 0; j < length(); j++) {
+                    if (i == j || copy.get(i, j) == Integer.MAX_VALUE) {
+                        copy.set(i, j, 0);
+                    } else {
+                        copy.set(i, j, Integer.MAX_VALUE);
+                    }
+                }
             }
             return copy;
         }
@@ -67,11 +86,11 @@ public class TheTwoRoutes {
     public static void main(String[] args) {
         int size = 5;
         Graph original = new Graph(size);
-        original.vertexes[0][1] = Integer.MAX_VALUE;
-        original.vertexes[0][4] = Integer.MAX_VALUE;
-        original.vertexes[1][3] = Integer.MAX_VALUE;
-        original.vertexes[2][4] = Integer.MAX_VALUE;
-        original.vertexes[3][4] = Integer.MAX_VALUE;
+        original.uSet(0, 1, Integer.MAX_VALUE);
+        original.uSet(0, 4, Integer.MAX_VALUE);
+        original.uSet(1, 3, Integer.MAX_VALUE);
+        original.uSet(2, 4, Integer.MAX_VALUE);
+        original.uSet(3, 4, Integer.MAX_VALUE);
 
         original.print();
         System.out.println("");
@@ -80,19 +99,7 @@ public class TheTwoRoutes {
         new TheTwoRoutes().breadthFirstSearch(copy, 0);
         copy.print();
 
-        Graph road = original.copy();
-        for (int i = 0; i < original.length(); i++) {
-            for (int j = 0; j < original.length(); j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (road.get(i, j) == 0) {
-                    road.set(i, j, Integer.MAX_VALUE);
-                } else {
-                    road.set(i, j, 0);
-                }
-            }
-        }
+        Graph road = original.reverse();
         System.out.println("");
 
         new TheTwoRoutes().breadthFirstSearch(road, 0);
