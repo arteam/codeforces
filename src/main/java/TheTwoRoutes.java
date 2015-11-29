f43fd10205352d2adefec7eea30a4aad0e9533b2
Artem Prigoda <aprigoda@bcc.ru>
2015-11-29 21:55:47 +0300

    Find the maximum between a railroad way and road way

--- src/main/java/TheTwoRoutes.java
+++ src/main/java/TheTwoRoutes.java
@@ -1,40 +1,18 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /**
  * Date: 11/29/15
  * Time: 4:18 PM
- *
- * @author Artem Prigoda
  */
 public class TheTwoRoutes {
-
-    public int breadthFirstSearch(Graph graph, int root) {
-        Queue<Integer> candidates = new ArrayDeque<>();
-        candidates.add(root);
-        while (!candidates.isEmpty()) {
-            Integer candidate = candidates.poll();
-            for (int i = 0; i < graph.length(); i++) {
-                if (graph.get(candidate, i) == Integer.MAX_VALUE) {
-                    graph.set(candidate, i, graph.get(root, candidate) + 1);
-                    graph.set(i, candidate, graph.get(root, candidate) + 1);
-                    if (graph.get(root, i) == 0) {
-                        graph.set(root, i, graph.get(root, candidate) + 1);
-                        graph.set(i, root, graph.get(root, candidate) + 1);
-                    }
-                    candidates.add(i);
-                }
-            }
-        }
-        return 0;
-    }
 
     private static class Graph {
         private int[][] vertexes;
 
         public Graph(int size) {
-            this.vertexes = new int[5][5];
+            this.vertexes = new int[size][size];
         }
 
         public int length() {
@@ -66,7 +44,7 @@ public class TheTwoRoutes {
             Graph copy = copy();
             for (int i = 0; i < length(); i++) {
                 for (int j = 0; j < length(); j++) {
-                    if (i == j || copy.get(i, j) == Integer.MAX_VALUE) {
+                    if (i == j || copy.get(i, j) > 0) {
                         copy.set(i, j, 0);
                     } else {
                         copy.set(i, j, Integer.MAX_VALUE);
@@ -83,26 +61,51 @@ public class TheTwoRoutes {
         }
     }
 
-    public static void main(String[] args) {
-        int size = 5;
-        Graph original = new Graph(size);
-        original.uSet(0, 1, Integer.MAX_VALUE);
-        original.uSet(0, 4, Integer.MAX_VALUE);
-        original.uSet(1, 3, Integer.MAX_VALUE);
-        original.uSet(2, 4, Integer.MAX_VALUE);
-        original.uSet(3, 4, Integer.MAX_VALUE);
+    private int breadthFirstSearch(Graph graph, int root) {
+        Queue<Integer> candidates = new ArrayDeque<>();
+        candidates.add(root);
+        while (!candidates.isEmpty()) {
+            Integer candidate = candidates.poll();
+            for (int i = 0; i < graph.length(); i++) {
+                if (graph.get(candidate, i) == Integer.MAX_VALUE) {
+                    graph.set(candidate, i, graph.get(root, candidate) + 1);
+                    graph.set(i, candidate, graph.get(root, candidate) + 1);
+                    if (graph.get(root, i) == 0) {
+                        graph.set(root, i, graph.get(root, candidate) + 1);
+                        graph.set(i, root, graph.get(root, candidate) + 1);
+                    }
+                    candidates.add(i);
+                }
+            }
+        }
+        return graph.get(0, graph.length() - 1);
+    }
 
-        original.print();
-        System.out.println("");
+    public int shortestPath(int vertexes, int amountEdges, List<String> edges) {
+        Graph graph = new Graph(vertexes);
+        for (String edge : edges) {
+            String[] parts = edge.split(" ");
+            int first = Integer.parseInt(parts[0]);
+            int second = Integer.parseInt(parts[1]);
+            graph.uSet(first - 1, second - 1, Integer.MAX_VALUE);
+        }
 
-        Graph copy = original.copy();
-        new TheTwoRoutes().breadthFirstSearch(copy, 0);
-        copy.print();
+        Graph reversed = graph.reverse();
+        int railroadPath = breadthFirstSearch(graph, 0);
+        int roadPath = breadthFirstSearch(reversed, 0);
+        return railroadPath == 0 || roadPath == 0 ? -1 : Math.max(railroadPath, roadPath);
+    }
 
-        Graph road = original.reverse();
-        System.out.println("");
-
-        new TheTwoRoutes().breadthFirstSearch(road, 0);
-        road.print();
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] parts = reader.readLine().split(" ");
+        int vertexes = Integer.parseInt(parts[0]);
+        int amountEdges = Integer.parseInt(parts[1]);
+        List<String> edges = new ArrayList<>();
+        for (int i = 0; i < amountEdges; i++) {
+            edges.add(reader.readLine());
+        }
+        int shortestPath = new TheTwoRoutes().shortestPath(vertexes, amountEdges, edges);
+        System.out.println(shortestPath);
     }
 }
