f5d578d1249a58e70a53de93cb83f2ac99c68f41
Artem Prigoda <arteamon@gmail.com>
2018-04-01 11:36:57 +0200

    Add JUnit5 parametrized tests

--- src/main/java/ModularExponentiation.java
+++ src/main/java/ModularExponentiation.java
@@ -4,11 +4,15 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class ModularExponentiation {
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        BigInteger m = new BigInteger(reader.readLine());
-        BigInteger result = m.mod(BigInteger.valueOf(2).pow(n));
-        System.out.println(result);
+        int m = Integer.parseInt(reader.readLine());
+        System.out.println(new ModularExponentiation().run(n, m));
+    }
+
+    public int run(int n, int m) {
+        return BigInteger.valueOf(m).mod(BigInteger.valueOf(2).pow(n)).intValue();
     }
 }
