8d51de2ffce53c5a7c233a6136248faefd44c963
Artem Prigoda <arteamon@gmail.com>
2018-04-01 14:41:55 +0200

    Add CompatiblePair

---
+++ src/test/java/CompatiblePairTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+class CompatiblePairTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "'2 2\n20 18\n2 14', 252",
+            "'5 3\n-1 0 1 2 3\n-1 0 1', 2"
+    })
+    public void test(String input, int output) throws IOException {
+        Assertions.assertEquals(output, new CompatiblePair().solve(new BufferedReader(new StringReader(input))));
+    }
+}
