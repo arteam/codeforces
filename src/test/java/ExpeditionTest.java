1d3df3b763ff792b22189529feb7e198b5a517dc
Artem Prigoda <arteamon@gmail.com>
2018-07-28 22:06:21 +0200

    Add Expedition

---
+++ src/test/java/ExpeditionTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+class ExpeditionTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 1 5 2 1 1 1 2 5 7 2, 2",
+            "100, 1, 0",
+            "2, 5 4 3 2 1, 1",
+            "3, 42 42 42 42 42 42 42 42 42, 3",
+            "1, 100, 1",
+    })
+    void test(int participants, String input, int tenure) {
+        Assertions.assertEquals(tenure, new Expedition().run(participants,
+                Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt)
+                        .toArray()));
+    }
+}
