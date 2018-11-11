9af1b481cafe96bbd8f22ebca55a681e05c3623e
Artem Prigoda <arteamon@gmail.com>
2018-11-11 18:29:23 +0100

    Add BerOs

---
+++ src/test/java/BerOsTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+class BerOsTest {
+
+    @Test
+    void testFind1() {
+        List<String> results = new BerOs().find(Arrays.asList("test",
+                "contests",
+                "test.",
+                ".test"), Arrays.asList(
+                "ts",
+                ".",
+                "st.",
+                ".test",
+                "contes.",
+                "st"
+        ));
+        Assertions.assertEquals(Arrays.asList(
+                "1 contests",
+                "2 test.",
+                "1 test.",
+                "1 .test",
+                "0 -",
+                "4 test"
+        ), results);
+    }
+
+    @Test
+    void testFind2() {
+        List<String> results = new BerOs().find(Arrays.asList(
+                ".a",
+                "a.",
+                "aa",
+                "a",
+                "aaa"
+        ), Arrays.asList(
+                ".",
+                "a",
+                "aa",
+                ".a",
+                "a.",
+                "..",
+                "aaa",
+                ".aa",
+                "      ..a",
+                "a.."
+        ));
+        Assertions.assertEquals(Arrays.asList("2 .a", "5 aa", "2 aa", "1 .a", "1 a.", "0 -",
+                "1 aaa", "0 -", "0 -", "0 -"), results);
+    }
+}
