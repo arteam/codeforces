7e17e60b4a95c3166bedf0bde7f59580cfb2d3fd
Artem Prigoda <arteamon@gmail.com>
2018-12-25 21:59:33 +0100

    Add Task7

---
+++ src/test/java/advent2018/Task7Test.java
@@ -0,0 +1,18 @@
+package advent2018;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Task7Test {
+
+    @Test
+    void solve() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/advent2018/task7.txt")))) {
+            Assertions.assertEquals("MNOUBYITKXZFHQRJDASGCPEVWL", new Task7().solve(reader));
+        }
+    }
+}
