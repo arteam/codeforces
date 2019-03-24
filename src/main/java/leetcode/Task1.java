f5b22565785263abee3c10be5ff10ed841e4f9a0
Artem Prigoda <artem.prigoda@goeuro.com>
2019-03-24 15:07:48 +0100

    Improve leetcode solutions

--- src/main/java/leetcode/Task1.java
+++ src/main/java/leetcode/Task1.java
@@ -1,21 +1,27 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Task1 {
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int[] twoSum = new Task1().twoSum(nums, 9);
+        //int[] nums = {2, 7, 11, 15};
+        int[] nums = {3, 2, 4};
+        int[] twoSum = new Task1().twoSum(nums, 6);
         System.out.println(Arrays.toString(twoSum));
     }
 
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indexedNums = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            indexedNums.put(nums[i], i);
+        }
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            Integer indexToTarget = indexedNums.get(target - nums[i]);
+            if (indexToTarget != null && indexToTarget != i) {
+                return new int[]{i, indexToTarget};
             }
         }
         return new int[]{};
--- src/main/java/leetcode/Task167.java
+++ src/main/java/leetcode/Task167.java
@@ -5,19 +5,24 @@ import java.util.Arrays;
 public class Task167 {
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int[] twoSum = new Task167().twoSum(nums, 9);
+        int[] nums = {1, 2, 3, 4, 4, 9, 56, 90};
+        int[] twoSum = new Task167().twoSum(nums, 8);
         System.out.println(Arrays.toString(twoSum));
     }
 
     public int[] twoSum(int[] nums, int target) {
-        int higherBound = Math.max(Math.min(Math.abs(Arrays.binarySearch(nums, target)), nums.length - 1), 1);
-        for (int i = 0; i < higherBound; i++) {
-            for (int j = i + 1; j < higherBound + 1; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i + 1, j + 1};
+        for (int i = 0; i < nums.length - 1; i++) {
+            int indexOfAddableNumber = Arrays.binarySearch(nums, target - nums[i]);
+            if (indexOfAddableNumber > 0) {
+                if (indexOfAddableNumber > i) {
+                    return new int[]{i + 1, indexOfAddableNumber + 1};
+                }
+                if (indexOfAddableNumber == i) {
+                    return new int[]{i + 1, i + 2};
                 }
             }
+
+
         }
         return new int[]{};
     }
