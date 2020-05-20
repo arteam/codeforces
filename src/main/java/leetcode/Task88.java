d4be8506aa86795ccc641483a63bb277b31a8e8b
Artem Prigoda <arteamon@gmail.com>
2020-05-20 12:21:22 +0200

    Add some tasks

---
+++ src/main/java/leetcode/Task88.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class Task88 {
+
+    public static void main(String[] args) {
+        new Task88().merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3);
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+}
