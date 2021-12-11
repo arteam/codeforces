77bfb34b8b54532dbd4c8af2282d6e3f30e99b3c
Artem Prigoda <arteamon@gmail.com>
2021-12-11 19:18:21 +0100

    Add Task11

---
+++ src/main/java/advent2021/Task_11.java
@@ -0,0 +1,56 @@
+package advent2021;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Task_11 {
+
+    private static final int SIZE = 10;
+
+    public static void main(String[] args) {
+        int[][] map = new BufferedReader(new InputStreamReader(System.in)).lines()
+                .map(s -> s.chars().map(Character::getNumericValue).toArray())
+                .toArray(int[][]::new);
+        MutableInt flashes = new MutableInt();
+        for (int step = 0; step < 100; step++) {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    map[i][j]++;
+                }
+            }
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (map[i][j] > 9) {
+                        flash(i, j, map, flashes);
+                    }
+                }
+            }
+        }
+        System.out.println(flashes.value);
+    }
+
+    static void flash(int i, int j, int[][] map, MutableInt flashes) {
+        flashes.value++;
+        map[i][j] = 0;
+        for (int stepI : List.of(0, -1, 1)) {
+            for (int stepJ : List.of(0, -1, 1)) {
+                int ni = i + stepI;
+                int nj = j + stepJ;
+                if (ni < 0 || ni >= SIZE || nj < 0 || nj >= SIZE) {
+                    continue;
+                }
+                if (map[ni][nj] == 0) {
+                    continue;
+                }
+                if (++map[ni][nj] > 9) {
+                    flash(ni, nj, map, flashes);
+                }
+            }
+        }
+    }
+
+    static class MutableInt {
+        int value;
+    }
+}
